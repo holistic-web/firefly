098f506d39d0a44e3e1ed6da40cbca59b8c65611
michaelfitzhavey <michael.fitzhavey@gmail.com>
2018-08-18 16:38:45 +0100

    fix: wire to use new spotify library

--- src/vuex/modules/spotifyStore.js
+++ src/vuex/modules/spotifyStore.js
@@ -1,15 +1,15 @@
 /**
  * Spotify module
- * 	- works with Spotfy npm package https://www.npmjs.com/package/spotify
+ * 	- works with Spotfy npm package https://www.npmjs.com/package/spotify-web-api-js
  */
 
-import Spotify from 'node-spotify-api';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi();
 
 // #TODO: move this to a .env file and reset for security...
-const spotify = new Spotify({
-	id: '10de6a8d8aec44aaa4d8c5a03c85be88',
-	secret: 'f0787962b898434eaafad7e60c297488'
-});
+spotifyApi.setAccessToken('f0787962b898434eaafad7e60c297488');
+
 
 export default {
 	namespaced: true,
@@ -22,8 +22,9 @@ export default {
 		}
 	},
 	actions: {
-		async fetchResults({ commit }, { type, query }) {
-			const results = await spotify.search({ type, query });
+		async fetchResults({ commit }, { queryTerm }) {
+			const limit = 20;
+			const results = await spotifyApi.searchTracks({ queryTerm, limit });
 			commit('SET_RESULTS', results.data);
 			return results;
 		}
