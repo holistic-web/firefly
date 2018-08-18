7d0dc907ad68d49632211be9c80a664ad2e9924f
michaelfitzhavey <michael.fitzhavey@gmail.com>
2018-08-18 16:29:55 +0100

    swap spotify module used

--- src/vuex/modules/spotifyStore.js
+++ src/vuex/modules/spotifyStore.js
@@ -3,7 +3,13 @@
  * 	- works with Spotfy npm package https://www.npmjs.com/package/spotify
  */
 
-import spotify from 'spotify';
+import Spotify from 'node-spotify-api';
+
+// #TODO: move this to a .env file and reset for security...
+const spotify = new Spotify({
+	id: '10de6a8d8aec44aaa4d8c5a03c85be88',
+	secret: 'f0787962b898434eaafad7e60c297488'
+});
 
 export default {
 	namespaced: true,
