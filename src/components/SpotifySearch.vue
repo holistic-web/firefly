fe7c217e9a005ce74f0838683991df578ff9765f
michaelfitzhavey <michael.fitzhavey@gmail.com>
2018-08-18 16:24:02 +0100

    feat: create basic search inputs and display results

--- src/components/SpotifySearch.vue
+++ src/components/SpotifySearch.vue
@@ -1,21 +1,69 @@
 <template>
 	<section class="Spotify">
-		Spotify
+
+		<section class="Spotify__search">
+			<b-form-group
+				class="Spotify__searchTerm"
+				description="Spotify track search"
+				label="Enter a searchphrase"
+				label-for="spotifySearchTerm">
+				<b-form-input
+					id="spotifySearchTerm"
+					v-model.trim="searchTerm"/>
+			</b-form-group>
+			<b-button
+				class="Spotify__searchButton"
+				variant="outline-primary"
+				@click="fetch"
+				v-text="'Search'"/>
+		</section>
+
+		{{results}}
+
 	</section>
 </template>
 
 <script>
-export default {
-	computed: {
+import { mapGetters, mapActions } from 'vuex';
 
+export default {
+	data() {
+		return {
+			searchTerm: null
+		};
+	},
+	computed: {
+		...mapGetters({
+			results: 'spotify/results'
+		})
 	},
 	methods: {
-
+		...mapActions({
+			fetchResults: 'spotify/fetchResults'
+		}),
+		async fetch() {
+			this.fetchResults({
+				type: 'track',
+				query: this.searchTerm
+			});
+		}
 	}
 };
 </script>
 
 <style lang="scss">
 
+.Spotify {
+
+	&__searchTerm {
+		display: inline-block;
+		width: 80%;
+	}
+
+	&__searchButton {
+		float: right;
+		margin-top: 2rem;
+	}
+}
 
 </style>
