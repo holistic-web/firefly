cce647ea5b2f3160f83384fd6d7bad7405ccbf08
michaelfitzhavey <michael.fitzhavey@gmail.com>
2018-08-21 01:49:52 +0100

    feat: style search badges

--- src/containers/Search/components/SearchInput.vue
+++ src/containers/Search/components/SearchInput.vue
@@ -1,22 +1,40 @@
 <template>
 	<section class="Search">
 
-		<b-form-group
-			class="Search__term"
-			description="Enter a searchphrase"
-			:label="label"
-			label-for="searchTerm">
-			<b-form-input
-				id="searchTerm"
-				v-model.trim="searchTerm"
-				@keydown.enter.native="submit"/>
-		</b-form-group>
+		<div class="Search__inputs">
+			<b-form-group
+				class="Search__term"
+				description="Enter a searchphrase"
+				:label="label"
+				label-for="searchTerm">
+				<b-form-input
+					id="searchTerm"
+					v-model.trim="searchTerm"
+					@keydown.enter.native="submit"/>
+			</b-form-group>
+			<b-button
+				class="Search__button"
+				variant="outline-primary"
+				@click="submit"
+				v-text="'Search'"/>
+		</div>
 
-		<b-button
-			class="Search__button"
-			variant="outline-primary"
-			@click="submit"
-			v-text="'Search'"/>
+		<div class="Search__badges">
+			<span>Currently Supports:</span>
+			<b-badge
+				variant="danger"
+				href="https://www.youtube.com/"
+				target="_blank">
+				Youtube
+			</b-badge>
+			<b-badge
+				variant="success"
+				href="https://www.spotify.com/"
+				target="_blank">
+				Spotify
+			</b-badge>
+		</div>
+
 
 	</section>
 </template>
@@ -44,19 +62,39 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../../../settings';
+
+$highlightColour: $Highlight-Colour;
+
+$searchTermWidth: 400px;
 
 .Search {
 	float: left;
 	width: 100%;
 
+	&__inputs {
+		float: left;
+		min-width: $searchTermWidth + 64px;
+	}
+
 	&__term {
-		display: inline-block;
+		float: left;
 		width: 80%;
+		max-width: $searchTermWidth;
 	}
 
 	&__button {
 		float: right;
+		margin-left: 1rem;
 		margin-top: 2rem;
+	}
+
+	&__badges {
+		color: $highlightColour;
+		position: absolute;
+		left: 250px;
+		top: -20px;
+		transform: rotate(5deg);
 	}
 
 }
