18ae56c382424c03e15e78d59946d49b7d8ca6e3
michaelfitzhavey <michael.fitzhavey@gmail.com>
2018-08-26 13:06:19 +0100

    feat: add spotify auth scopes to read/modify playback state and add function scaffold for spotify contorls

--- src/lib/Spotify.js
+++ src/lib/Spotify.js
@@ -9,7 +9,7 @@ export default {
 		let path = 'https://accounts.spotify.com/authorize';
 		path += '?response_type=token';
 		path += '&client_id=10de6a8d8aec44aaa4d8c5a03c85be88';
-		path += '&scope=user-modify-playback-state';
+		path += '&scope=user-modify-playback-state user-read-playback-state';
 		if (window.location.hostname.includes('localhost')) {
 			path += '&redirect_uri=http://localhost:3001/token';
 		} else {
@@ -28,17 +28,41 @@ export default {
 		return items;
 	},
 
+	// #Todo: functions below here have not yet been implemented properly
+	// we need to do something with the Player object (maybe avaialble through
+	// window.Spotify.player thanks to index.html)
+
+
+	createPlayer: async token => {
+		const player = new window.Spotify.Player({
+			name: 'Carly Rae Jepsen Player',
+			getOAuthToken: callback => {
+				callback(token);
+			},
+			volume: 0.5
+		});
+		const result = player.connect();
+		console.log(player);
+		console.log(result);
+		return result;
+	},
+
 	play: async () => {
 		// #Todo: implement spotify play pause
 		// Make a global store to manage player state (playing, play/pause methods, including queues)
-		console.log(spotifyApi);
+		const playBackState = spotifyApi.getMyCurrentPlaybackState();
+		console.log(playBackState);
+		spotifyApi.play();
 	},
 
 	pause: async () => {
 		// #Todo: implement spotify play pause
 		// Make a global store to manage player state (playing, play/pause methods, including queues)
 		console.log(spotifyApi);
+	},
+
+	getDevices: async () => {
+		const result = await spotifyApi.getDevices();
+		return result;
 	}
 };
-
-// This is vuex, it's not usual vue
